d9e9c1d141298d363b20b9417011333c2b6897af
aaron549 <sunaaron549@gmail.com>
2021-05-11 16:01:30 -0400

    .

---
+++ src/js/question.js
@@ -0,0 +1,49 @@
+//quiz game questions
+const prompt1 = [
+  {
+    question: "Which key is a homerow key?",
+    choice: {
+      a: "L",
+      b: "B",
+      c: "Q",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Which country have the shortest space keys?",
+    choice: {
+      a: "Japan",
+      b: "Canada",
+      c: "Italy",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Which of the following is the father of all keyboards?",
+    choice: {
+      a: "AEK M0115",
+      b: "Cherry G80-3000SAV",
+      c: "IBM model M",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Who invented the typewriter?",
+    choice: {
+      a: "Nikola Tesla",
+      b: "Christopher Latham Sholes",
+      c: "Thomas Edison",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Which country does not have its own unique keyboard layout?",
+    choice: {
+      a: "Portugal",
+      b: "Austria",
+      c: "Belgium",
+      d: "Switzerland",
+    },
+    correctAnswer: "b",
+  },
+];
