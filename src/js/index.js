fce33c35f29f11613989b63d0a2d01767c454e5d
aaron549 <sunaaron549@gmail.com>
2021-05-12 11:53:03 -0400

    8

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import { prompt1 } from "./question";
+
 (function () {
   //"global scope and ensures quiz wont interfere with other script"
   function quizgame() {
@@ -92,55 +94,6 @@
   const containerquiz = document.getElementById("quiz"); //html element storage, and references them in variables
   const containerR = document.getElementById("results");
   const submitbtn = document.getElementById("submit");
-  const prompt1 = [
-    //quiz game questions
-    {
-      question: "Which key is a homerow key?",
-      choice: {
-        a: "L",
-        b: "B",
-        c: "Q",
-      },
-      correctAnswer: "a",
-    },
-    {
-      question: "Which country have the shortest space keys?",
-      choice: {
-        a: "Japan",
-        b: "Canada",
-        c: "Italy",
-      },
-      correctAnswer: "a",
-    },
-    {
-      question: "Which of the following is the father of all keyboards?",
-      choice: {
-        a: "AEK M0115",
-        b: "Cherry G80-3000SAV",
-        c: "IBM model M",
-      },
-      correctAnswer: "c",
-    },
-    {
-      question: "Who invented the typewriter?",
-      choice: {
-        a: "Nikola Tesla",
-        b: "Christopher Latham Sholes",
-        c: "Thomas Edison",
-      },
-      correctAnswer: "b",
-    },
-    {
-      question: "Which country does not have its own unique keyboard layout?",
-      choice: {
-        a: "Portugal",
-        b: "Austria",
-        c: "Belgium",
-        d: "Switzerland",
-      },
-      correctAnswer: "b",
-    },
-  ];
   //starts
   quizgame();
   //pagination
